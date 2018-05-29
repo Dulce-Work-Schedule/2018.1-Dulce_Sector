2fc2c6718bcd9bbb255496abb2e4e221cf2be9a9
Guilherme Deusdará <guibanci@gmail.com>
2018-05-29 20:36:25 -0300

    Fix #3 #8 criando ambiente de dev e componentizando
    Signed-off-by: Beatriz Hanae <beatriz.hanae@gmail.com>
    Signed-off-by: Guilherme Deusdará <guibanci@gmail.com>
    Signed-off-by: Isaque Alves <isaquealvesdl@gmail.com>
    
    Co-Authored-by: Beatriz Hanae <beatriz.hanae@gmail.com>
    Co-Authored-by: Guilherme Deusdará <guibanci@gmail.com>
    Co-Authored-by: Isaque Alves <isaquealvesdl@gmail.com>

--- Server/sector.js
+++ Server/sector.js
@@ -1,35 +1,17 @@
 require('seneca')()
  .use("entity")
  .use('mongo-store',{
-    name:'dataBaseHospitals',
-    host:'mongo',
-    port:27017
+    name: process.env.MONGO_DATABASE,
+    host:process.env.MONGO_HOST,
+    port: process.env.MONGO_PORT
   })
  .use('seneca-amqp-transport')
+ .use('_sector')
  .listen({
     type:'amqp',
     pin:'role:sector',
-    port: 5672,
-    username: 'guest',
-    password: 'guest',
-    url: 'amqp://rabbitmq',
+    port: process.env.RABBITMQ_PORT,
+    username: process.env.RABBITMQ_DEFAULT_USER,
+    password: process.env.RABBITMQ_DEFAULT_PASS,
+    url: 'amqp://'+ process.env.RABBITMQ_HOST
 })
-
-  .add('role:sector,cmd:create', function create (msg,respond) {
-    var sector = this.make('sectors')
-    sector.name = msg.name
-    sector.save$(function(err,sector){
-      respond(null,sector)
-    })
-  })
-
-  .add('role:sector, cmd:listSector', function listSector(msg, respond){
-    var sector = this.make('sectors');
-    sector.list$({all$:true}, function(error,sector){
-      respond(null,sector);
-    });
-  })
-
-  .add('role:sector, cmd:error', function error(msg, respond){
-    respond(null, {success:false, message: 'acesso negado'});
-  })
